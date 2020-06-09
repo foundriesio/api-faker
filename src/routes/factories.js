1b7a438907dbee8e10b4784914f8cc52afa4ee5d
Milo Casagrande <milo@foundries.io>
2020-06-09 11:27:43 +0200

    Add factories status handler
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/routes/factories.js
+++ src/routes/factories.js
@@ -7,7 +7,71 @@ You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2
 */
 
 import express from 'express';
+import faker from 'faker';
+
+const MAX_DEVICES = 60;
 
 const router = express.Router();
+
+/**
+ * @param {Number} latestVersion
+ * @returns {Array}
+ */
+function generateTargets(latestVersion) {
+  const totalTargets = faker.random.number({ min: 1, max: 10 });
+  const targets = new Array(totalTargets);
+
+  for (let i = 0; i < totalTargets; i++) {
+    // eslint-disable-next-line security/detect-object-injection
+    targets[i] = {
+      devices: faker.random.number({ min: 1, max: 10 }),
+      version: faker.random.number({ min: 1, max: latestVersion }),
+    };
+  }
+
+  return targets;
+}
+
+/**
+ * @returns {String}
+ */
+function generateTagName() {
+  return faker.random.arrayElement([
+    '',
+    'promoted',
+    'postmerge',
+    faker.random.word().toLowerCase(),
+  ]);
+}
+
+/**
+ * @returns {Array}
+ */
+function generateTags() {
+  const totalTags = faker.random.number({ min: 1, max: 10 });
+  const tags = new Array(totalTags);
+
+  for (let i = 0; i < totalTags; i++) {
+    // eslint-disable-next-line security/detect-object-injection
+    tags[i] = {
+      name: generateTagName(),
+      'devices-total': faker.random.number({ min: 1, max: 20 }),
+      'devices-online': faker.random.number({ min: 1, max: 20 }),
+      'devices-on-latest': faker.random.number({ min: 1, max: 10 }),
+      'latest-target': faker.random.number(),
+      targets: generateTargets(),
+    };
+  }
+
+  return tags;
+}
+
+router.get('/:factory/status', (req, res) => {
+  req.log.info(`Retrieving status for factory '${req.params.factory}'`);
+  res.json({
+    'total-devices': MAX_DEVICES,
+    tags: generateTags(),
+  });
+});
 
 export default router;
