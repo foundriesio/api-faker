30c54ee66322fbcdfe1fa80234b7ad064cb2dc75
Milo Casagrande <milo@foundries.io>
2020-11-12 09:56:48 +0100

    update: change jwt library and auth check
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -6,9 +6,17 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 */
 
-import jws from 'jws';
+import Future from 'bluebird';
+import jwt from 'jsonwebtoken';
 
-export function decodeJwtSignature(req, res, next) {
+import config from '../config';
+
+Future.promisifyAll(jwt);
+
+const cfg = config();
+const secret = cfg.get('faker');
+
+export async function decodeJwtSignature(req, res, next) {
   const authorizationHeader = req.headers['authorization'];
 
   if (!authorizationHeader) {
@@ -22,7 +30,7 @@ export function decodeJwtSignature(req, res, next) {
   }
 
   try {
-    const payload = JSON.parse(jws.decode(authValue).payload);
+    const payload = await jwt.verifyAsync(authValue, secret);
     res.locals.user = payload.id;
   } catch (ex) {
     req.log.error('Error decoding JWT payload');
